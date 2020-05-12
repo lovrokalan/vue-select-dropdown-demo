18056ff085941039b01632d797b06140e54a1784
Lovro Kalan <lovro.kalan1@gmail.com>
2020-05-12 21:06:57 +0200

    Basic closed empty dropdown div element created.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DropdownSelect>
+    </DropdownSelect>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DropdownSelect from './components/DropdownSelect.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DropdownSelect
   }
 }
 </script>
 
 <style>
+/* Lato in font-weight 600 not available on google fonts */
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 50px;
 }
+
+html,
+body {
+  background-color: #EDF2F7;
+}
+
 </style>
---
+++ src/components/DropdownSelect.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dropdown-container">
+    <span
+      class="closed-dropdown-text"
+    >
+      Select an item
+    </span>
+
+    <div
+      class="inline cursor-pointer z-1"
+    >
+      <img
+        class="float-right chevron-icon"
+        src="../assets/chevron-down.svg"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownSelect',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+.dropdown-container {
+  background-color: #fff;
+  border-radius: 8px;
+  -webkit-box-shadow: 0px 10px 15px rgba(35, 78, 82, 0.1);
+  -moz-box-shadow: 0px 10px 15px rgba(35, 78, 82, 0.1);
+  box-shadow: 0px 10px 15px rgba(35, 78, 82, 0.1);
+  padding: 16px;
+  cursor: pointer;
+}
+
+.closed-dropdown-text {
+  font-size: 16px;
+  line-height: 24px;
+  color: #A0AEC0;
+}
+
+.chevron-icon {
+  margin-top: 9px;
+}
+</style>
